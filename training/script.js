7e29b28dd9490fc2ae86c1ed71fb5b655f40f9d8
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-05-16 22:43:55 +0200

    feat: add base url

--- training/script.js
+++ training/script.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 (() => {
     const mds = fs.readdirSync('.').filter(p => p.endsWith('.md')).filter(p => !p.startsWith('README'));
     mds.forEach(md => {
+        const base = md.replace('.md', '');
         console.log(`building ${md}`)
-        exec(`npm run build -- ${md} --out dist/${md.replace('.md', '')}`)
+        exec(`npm run build -- ${md} --base ${base} --out dist/${base}`)
     })
 })()
