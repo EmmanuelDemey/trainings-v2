2a70e3e9235c76dcce3f63ccf4746c19f43acdd9
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-05-16 22:19:49 +0200

    feat: deploy to netlify

---
+++ training/script.js
@@ -0,0 +1,10 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+
+(() => {
+    const mds = fs.readdirSync('.').filter(p => p.endsWith('.md')).filter(p => !p.startsWith('README'));
+    mds.forEach(md => {
+        console.log(`building ${md}`)
+        exec(`npm run build -- ${md} --out dist/${md.replace('.md', '')}`)
+    })
+})()
