b7dcd53b66e2aa45a94317fe73fe9ca3e5e4b56b
Nicolas Laval <nicolas.laval@making-sense.info>
2024-05-27 18:36:31 +0200

    PW3.1

--- react-trainings-v2/src/App.test.tsx
+++ react-trainings-v2/src/App.test.tsx
@@ -1,17 +1,19 @@
-import { describe, expect, test, beforeEach } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import App from './App'
- 
+import { describe, expect, test, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 describe('App', () => {
-    beforeEach(() => {
-        render(<App />);
-    });
+	beforeEach(() => {
+		render(<App />);
+	});
 
-    test("should check title", () => {  
-        expect(screen.getByRole('heading', { level: 1, name: 'Hello World' })).toBeDefined();
-    })
+	test('should check title', () => {
+		expect(
+			screen.getByRole('heading', { level: 1, name: 'Hello World' })
+		).toBeDefined();
+	});
 
-    test("check p content", () => {
-        expect(screen.getByRole('paragraph').textContent).toContain('Bulma')
-    })
-})
+	test('check p content', () => {
+		expect(screen.getByRole('paragraph').textContent).toContain('Bulma');
+	});
+});
--- react-trainings-v2/src/App.tsx
+++ react-trainings-v2/src/App.tsx
@@ -1,14 +1,14 @@
 function App() {
-  return (
-    <section className="section">
-      <div className="container">
-        <h1 className="title">Hello World</h1>
-        <p className="subtitle">
-          My first website with <strong>Bulma</strong>!
-        </p>
-      </div>
-    </section>
-  );
+	return (
+		<section className="section">
+			<div className="container">
+				<h1 className="title">Hello World</h1>
+				<p className="subtitle">
+					My first website with <strong>Bulma</strong>!
+				</p>
+			</div>
+		</section>
+	);
 }
 
 export default App;
